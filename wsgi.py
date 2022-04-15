9ca0009ccee0b3c146e9d1054caa96d4db1310b2
Balwant <49509457+ballu1190@users.noreply.github.com>
2022-04-15 19:38:38 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
 from api import app
 #gunicorn --bind 0.0.0.0:5000 --workers=5 --threads=2 wsgi:app
 #waitress-serve --listen=127.0.0.1:8000 wsgi:app --threads=2
-heroku ps:scale web=2
 if __name__ == "__main__":
     app.run()
